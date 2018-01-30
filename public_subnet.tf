d92379fbe1fc1c2e06cb449ee16be3ceb6dacf82
Angel Villalain <angel.villalain@gmail.com>
2018-01-30 11:56:31 -0600

    Added support for public subnets

---
+++ public_subnet.tf
@@ -0,0 +1,39 @@
+resource "aws_subnet" "public" {
+  count = "${length(var.availability_zones)}"
+  cidr_block = "${var.subnet_cidr_blocks[var.availability_zones[count.index]]}"
+  availability_zone = "${element(var.availability_zones, count.index)}"
+  vpc_id = "${aws_vpc.this.id}"
+  tags {
+    Name = "${format("%s-%s-subnet", var.vpc_name, element(var.availability_zones, count.index))}"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  count = "${length(var.availability_zones) > 0 ? 1 : 0}"
+  vpc_id = "${aws_vpc.this.id}"
+  tags {
+    Name = "${format("%s-igw", var.vpc_name)}"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  count = "${length(var.availability_zones) > 0 ? 1 : 0}"
+  vpc_id = "${aws_vpc.this.id}"
+  tags {
+    Name = "${format("%s-public-route-table", var.vpc_name)}"
+  }
+}
+
+resource "aws_route" "public_route" {
+  count = "${length(var.availability_zones) > 0 ? 1 : 0}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+  gateway_id = "${aws_internet_gateway.this.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  depends_on = ["aws_route_table.public_route_table"]
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  count = "${length(var.availability_zones)}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+}
---
+++ spec/public_subnet_spec.rb
@@ -0,0 +1,35 @@
+require './spec/spec_helper'
+require 'json'
+
+tfvars = JSON.parse(File.read(File.absolute_path("spec/fixtures/test.json")))
+subnet_east_a = subnet("#{tfvars["vpc_name"]}-#{tfvars["availability_zones"][0]}-subnet")
+subnet_east_b = subnet("#{tfvars["vpc_name"]}-#{tfvars["availability_zones"][1]}-subnet")
+igw = internet_gateway("#{tfvars["vpc_name"]}-igw")
+route_table = route_table("#{tfvars["vpc_name"]}-public-route-table")
+
+describe subnet_east_a do
+  it { should exist }
+  it { should belong_to_vpc(tfvars["vpc_name"])}
+  its(:availability_zone) { should eq tfvars["availability_zones"][0]}
+
+end
+
+describe subnet_east_b do
+  it { should exist }
+  it { should belong_to_vpc(tfvars["vpc_name"])}
+  its(:availability_zone) { should eq tfvars["availability_zones"][1]}
+  its(:cidr_block) { should eq tfvars["subnet_cidr_blocks"][tfvars["availability_zones"][1]]}
+end
+
+describe igw do
+  it { should exist }
+  it { should be_attached_to("#{tfvars["vpc_name"]}") }
+end
+
+describe route_table do
+  it { should exist }
+  it { should belong_to_vpc(tfvars["vpc_name"]) }
+  it { should have_subnet(subnet_east_a.id) }
+  it { should have_subnet(subnet_east_b.id) }
+  it { should have_route("0.0.0.0/0").target(gateway: igw.id) }
+end
